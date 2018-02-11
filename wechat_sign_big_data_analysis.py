9c7ca92a64c5a8d74de768e4722ccf90f71fe504
chaos1984 <chaos1984@163.com>
2018-02-11 19:25:29 +0800

    ipynd

--- wechat_sign_big_data_analysis.py
+++ wechat_sign_big_data_analysis.py
@@ -71,7 +71,7 @@ def friendsstatic():
     text = ' '.join(Signatures)
     wordlist = jieba.cut(text, cut_all=True)
     word_space_split = ' '.join(wordlist)
-    coloring = np.array(Image.open("123.png")) #词云的背景和颜色。这张图片在本地。
+    coloring = np.array(Image.open("123.jpg")) #词云的背景和颜色。这张图片在本地。
     my_wordcloud = WordCloud(background_color="white", max_words=2000,
     mask=coloring, max_font_size=60, random_state=42, scale=2,
     font_path="C:\Windows\Fonts\msyhl.ttc").generate(word_space_split) #生成词云。font_path="C:\Windows\Fonts\msyhl.ttc"指定字体，有些字不能解析中文，这种情况下会出现乱码。
